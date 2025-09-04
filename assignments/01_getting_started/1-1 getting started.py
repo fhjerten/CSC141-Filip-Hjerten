c2e2dfd6e5d7ba0df48ceb410db4ae130a9444ab
fhjerten <f.hjerten@gmail.com>
2025-09-04 11:56:34 -0400

    Add files via upload

---
+++ assignments/01_getting_started/1-1 getting started.py
@@ -0,0 +1,11 @@
+#01_01_python_org
+#my first assignment
+
+# - Easy “Downloads” section provides installers for major OSes; highlights the latest stable release and platform notes (e.g., Windows builds)
+
+# - “Developer’s Guide” explains how to contribute to CPython (issues, workflows, tests).
+
+# “Getting Started” (Beginners) page explains what Python is good for and how to learn it.
+
+message = "get ready for python!"
+print(message)
