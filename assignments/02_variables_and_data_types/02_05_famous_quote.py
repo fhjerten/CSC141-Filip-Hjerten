06f64e04434b20fe2b6dd34fe876c5993e1222ff
fhjerten <f.hjerten@gmail.com>
2025-09-09 11:37:31 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_05_famous_quote.py
@@ -0,0 +1,7 @@
+# I start with creating variables for the author and the quote
+
+author= "Nelson Mandela"
+
+quote= "Education is the most powerful weapon which you can use to change the world."
+
+print (f'{author} once said, "{quote}"')
