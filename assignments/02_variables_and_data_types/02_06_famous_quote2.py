df375d1b47b464df01db86d13d2224c174e97282
fhjerten <f.hjerten@gmail.com>
2025-09-09 11:42:18 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_06_famous_quote2.py
@@ -0,0 +1,13 @@
+# I start with creating a variable for the person
+
+famous_person = "Nelson Mandela"
+
+# I create a variable for the quote
+
+quote= "Education is the most powerful weapon which you can use to change the world."
+
+# I combine it to a message
+
+message= f'{famous_person} once said, "{quote}"'
+
+print (message)
