62eb4cf72fbfe984b64d866b9ee6f348900b854a
fhjerten <f.hjerten@gmail.com>
2025-09-09 12:10:25 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,9 @@
+# author : Filip
+# date : 2024-09-09
+# This program stores my favourite number in a variable and then prints it.
+
+favourite_number = 5 # This is my favourite number 5
+
+message = f"My favourite number is {favourite_number}" 
+
+print (message)
