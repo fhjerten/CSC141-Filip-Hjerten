21856dc807f0891dc3b22ac871ccd1042690e380
fhjerten <f.hjerten@gmail.com>
2025-09-09 12:06:55 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_10_favorite_number.py
@@ -0,0 +1,9 @@
+# I create a variable with my favourite number
+
+favourite_number = 5
+
+# I create a message which includes the variable
+
+message = f"My favourite number is {favourite_number}"
+
+print (message)
