28f6e538879ece6b79a6baf1c842b9335926d047
fhjerten <f.hjerten@gmail.com>
2025-09-09 12:25:58 -0400

    Add files via upload

---
+++ assignments/01_getting_started/01_02_hello_world_typos.py
@@ -0,0 +1,10 @@
+# This file shows typos that cause errors and typos that don’t.
+
+# Normal version
+#prnt("Hello World")
+
+# Typo that causes error (NameError, because 'prnt' is not defined)
+# prnt("Hello World")
+
+# Typo that does not cause an error (Python just prints the misspelled text)
+#print("Hello Wrold")
