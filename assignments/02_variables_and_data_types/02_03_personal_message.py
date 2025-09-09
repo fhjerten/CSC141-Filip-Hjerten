e4a9aa6066c6fe30032b5b801ebeeef24635e2eb
fhjerten <f.hjerten@gmail.com>
2025-09-09 11:29:35 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_03_personal_message.py
@@ -0,0 +1,9 @@
+#I start with creating a variable with a name
+
+name = "Mister Phil"
+
+#I am creating a message with the name variable
+
+message = f"hello {name}, would you like to learn some Python today?"
+
+print (message)
