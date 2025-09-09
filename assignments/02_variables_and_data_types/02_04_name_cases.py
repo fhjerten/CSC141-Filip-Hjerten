0a3479e56b4ed22ec725a786fa9b2b4e6f48ef19
fhjerten <f.hjerten@gmail.com>
2025-09-09 11:33:03 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_04_name_cases.py
@@ -0,0 +1,9 @@
+# I am creating a variable with a name
+
+name = "Mister Phil"
+
+# I am writing name in different cases
+
+print (name.lower()) #lowercase
+print (name.upper()) #uppercase
+print (name.title()) #titlecase
