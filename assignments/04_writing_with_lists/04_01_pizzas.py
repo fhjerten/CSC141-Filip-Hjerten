01a9fd073e9038734d8bcb4cfdb7a6f0e1e3be0e
fhjerten <f.hjerten@gmail.com>
2025-09-10 18:51:33 -0400

    Add files via upload

---
+++ assignments/04_writing_with_lists/04_01_pizzas.py
@@ -0,0 +1,9 @@
+# Make a list with at least three favorite pizzas
+pizzas = ["pepperoni", "margherita", "bbq chicken"]
+
+# Use a for loop to print sentences about each pizza
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+# Add one more line outside the loop
+print("\nI really love pizza!")
