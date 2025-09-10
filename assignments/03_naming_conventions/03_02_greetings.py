1d27b4d9f026f1cd15a2236f8bd5d3da2d8e8a41
fhjerten <f.hjerten@gmail.com>
2025-09-10 15:32:19 -0400

    Add files via upload

---
+++ assignments/03_naming_conventions/03_02_greetings.py
@@ -0,0 +1,9 @@
+# This is my list of friends
+
+names = ["Freddie", "Jason", "Ashton"]
+
+# I print a personalized message for each friend
+
+print ("Hello " + names[0] + "! would you like to hang out sometime?")
+print ("Hello " + names[1] + "! I hope you're having a great day!")
+print ("Hello " + names[2] + "! I want to catch up with you soon!")
