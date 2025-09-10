24fa49bc09c7ec31d84a29a34fb84ce108561bd2
fhjerten <f.hjerten@gmail.com>
2025-09-10 18:21:30 -0400

    Add files via upload

---
+++ assignments/03_naming_conventions/03_09_dinner_guests.py
@@ -0,0 +1,12 @@
+# Here is the guest list
+
+guests = ["Albert Einstein", "Marie Curie", "Isaac Newton"]
+
+# I print the invitations
+
+for guest in guests: 
+    print("Dear " + guest + ", you are invited to dinner at my place.")
+
+# I use len () to show how many people are invited
+
+print ("\nI am inviting " + str(len(guests))+ " people to dinner.")
