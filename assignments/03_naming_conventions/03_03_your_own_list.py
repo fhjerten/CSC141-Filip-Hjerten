8cf6d858c64ef77913598f474492932362c44df7
fhjerten <f.hjerten@gmail.com>
2025-09-10 15:35:46 -0400

    Add files via upload

---
+++ assignments/03_naming_conventions/03_03_your_own_list.py
@@ -0,0 +1,11 @@
+# List of my favourite vechiles
+
+cars = ["Vovlo", "Nagasaki", "BMW", "Audi"]
+
+# I print a message about each car
+
+print ("I would like to own a " + cars[0] + " car one day.")
+print ("I would like to own a " + cars[1] + " Motorcycle one day.")
+print ("I would like to own a " + cars[2] + " car one day.")
+print ("I would like to own a " + cars[3] + " car one day.")
+
