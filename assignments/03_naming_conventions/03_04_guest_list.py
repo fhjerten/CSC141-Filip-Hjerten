970dd9c5ed0028e3d52b4c2b734735df90b7d43d
fhjerten <f.hjerten@gmail.com>
2025-09-10 15:39:22 -0400

    Add files via upload

---
+++ assignments/03_naming_conventions/03_04_guest_list.py
@@ -0,0 +1,9 @@
+# List of dinner guests
+
+guests = ["Muhammed Ali", "Albert Einstein", "Isaac Newton"]
+
+# Print a message to each guest
+
+print ("dear " + guests[0] + ", you are invited to dinner at my place this Saturday at 7 PM.")
+print ("dear " + guests[1] + ", you are invited to dinner at my place this Saturday at 7 PM.")
+print ("dear " + guests[2] + ", you are invited to dinner at my place this Saturday at 7 PM.")
