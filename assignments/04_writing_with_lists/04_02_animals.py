1eb46c118df7374ae1e16ec8ba531c588c8581c3
fhjerten <f.hjerten@gmail.com>
2025-09-10 18:55:00 -0400

    Add files via upload

---
+++ assignments/04_writing_with_lists/04_02_animals.py
@@ -0,0 +1,17 @@
+# I Make a list with at least three animals that share a characteristic
+
+animals = ["dog", "cat", "rabbit"]
+
+# I Use a for loop to print different sentences about each animal
+
+for animal in animals:
+    if animal == "dog":
+        print("A dog would be a loyal companion.")
+    elif animal == "cat":
+        print("A cat would be an independent but loving pet.")
+    elif animal == "rabbit":
+        print("A rabbit would be a quiet and gentle pet.")
+
+# I add a line outside of the loop to write something common about all the animals.
+
+print("\nAny of these animals would make a great pet!")
