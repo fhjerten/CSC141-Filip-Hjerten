6903cadf8a327ea7a94a188d3fe292a7ff6f28b3
fhjerten <f.hjerten@gmail.com>
2025-09-11 12:11:00 -0400

    Add files via upload

---
+++ assignments/04_writing_with_lists/04_06_odd_numbers.py
@@ -0,0 +1,9 @@
+
+# I Make a list of the odd numbers from 1 to 20 using range()
+
+odd_numbers = list(range(1, 21, 2))
+
+# I Use a for loop to print each number
+
+for number in odd_numbers:
+    print(number)
