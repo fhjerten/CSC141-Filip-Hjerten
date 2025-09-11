efdc23125c6396ddc9f15b93e2814ac9f2226bdf
fhjerten <f.hjerten@gmail.com>
2025-09-11 11:39:23 -0400

    Add files via upload

---
+++ assignments/04_writing_with_lists/04_05_summing_a_million.py
@@ -0,0 +1,12 @@
+# I Make a list of numbers from 1 to 1,000,000
+
+numbers = list(range(1, 1_000_001))
+
+# I Check that the list starts at 1 and ends at 1,000,000
+
+print("Minimum value:", min(numbers))
+print("Maximum value:", max(numbers))
+
+# I Use sum() to add all the numbers together
+
+print("Sum of all numbers:", sum(numbers))
