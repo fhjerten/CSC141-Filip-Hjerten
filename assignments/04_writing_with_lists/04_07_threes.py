a193a2095803222c1c82faae8f05c53cd4ee31e2
fhjerten <f.hjerten@gmail.com>
2025-09-16 12:18:02 -0400

    Add files via upload

---
+++ assignments/04_writing_with_lists/04_07_threes.py
@@ -0,0 +1,8 @@
+# I Make a list of the multiples of 3, from 3 to 30
+
+multiples_of_three = list(range(3, 31, 3))
+
+# I use the for loop to print the numbers in the list
+
+for number in multiples_of_three:
+    print(number)
