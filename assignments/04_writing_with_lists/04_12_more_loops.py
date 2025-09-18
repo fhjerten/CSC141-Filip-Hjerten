f19d53d442a1a18753d8c314a27c8a9ac3a20d27
fhjerten <f.hjerten@gmail.com>
2025-09-18 11:54:56 -0400

    Add files via upload

---
+++ assignments/04_writing_with_lists/04_12_more_loops.py
@@ -0,0 +1,13 @@
+# My favorite pizzas
+pizzas = ["pepperoni", "margherita", "bbq chicken", "hawaiian"]
+
+# My friend's favorite pizzas
+friend_pizzas = ["pepperoni", "margherita", "bbq chicken", "veggie"]
+
+print("My favorite pizzas are:")
+for pizza in pizzas:
+    print(pizza)
+
+print("\nMy friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
