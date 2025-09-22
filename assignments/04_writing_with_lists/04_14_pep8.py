df30a3306be8ea1837efee8927927636a0697ac1
fhjerten <f.hjerten@gmail.com>
2025-09-22 14:00:21 -0400

    Add files via upload

---
+++ assignments/04_writing_with_lists/04_14_pep8.py
@@ -0,0 +1,6 @@
+# I skimmed PEP 8 style guide:#I skimmed PEP 8 style guide:
+# You have to use 4 spaces for indentation
+# Keep lines under 79 characters
+# Use lowercase_with_underscores for variables/functions
+# Use capwords for class names
+# Write clear comments and docstrings
