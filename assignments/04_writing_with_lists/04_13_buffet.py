9cffd9a02b581b67371cc2bacd0701065eb06970
fhjerten <f.hjerten@gmail.com>
2025-09-22 13:45:21 -0400

    Add files via upload

---
+++ assignments/04_writing_with_lists/04_13_buffet.py
@@ -0,0 +1,19 @@
+# I store 5 foods
+
+buffet_foods = ("Pizza", "Pasta", "Salad", "Soup", "Bread")
+
+# I Use a for loop to print each food
+
+print("Original menu:")
+for food in buffet_foods:
+    print(food)
+
+# If the restaurant changes its menu, I replace two items
+
+buffet_foods = ("Pizza", "Burger", "Salad", "Sushi", "Bread")
+
+# I print the revised menu
+
+print("\nRevised menu:")
+for food in buffet_foods:
+    print(food)
