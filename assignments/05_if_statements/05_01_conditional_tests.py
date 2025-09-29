2012ba7520cb43167a9940c652db2af0f5de8263
fhjerten <f.hjerten@gmail.com>
2025-09-29 11:38:28 -0400

    Add files via upload

---
+++ assignments/05_if_statements/05_01_conditional_tests.py
@@ -0,0 +1,34 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+age = 18
+print("\nIs age >= 18? I predict True.")
+print(age >= 18)
+
+print("\nIs age < 18? I predict False.")
+print(age < 18)
+
+fruit = 'apple'
+print("\nIs fruit == 'apple'? I predict True.")
+print(fruit == 'apple')
+
+print("\nIs fruit == 'banana'? I predict False.")
+print(fruit == 'banana')
+
+color = 'blue'
+print("\nIs color != 'red'? I predict True.")
+print(color != 'red')
+
+print("\nIs color == 'green'? I predict False.")
+print(color == 'green')
+
+number = 10
+print("\nIs number > 5? I predict True.")
+print(number > 5)
+
+print("\nIs number < 5? I predict False.")
+print(number < 5)
