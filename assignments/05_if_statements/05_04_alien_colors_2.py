7814faed69149318b5b679a85895df97d491ea50
fhjerten <f.hjerten@gmail.com>
2025-09-30 11:13:17 -0400

    Add files via upload

---
+++ assignments/05_if_statements/05_04_alien_colors_2.py
@@ -0,0 +1,17 @@
+
+
+# Version that runs the if block
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
+
+# Version that runs the else block
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
