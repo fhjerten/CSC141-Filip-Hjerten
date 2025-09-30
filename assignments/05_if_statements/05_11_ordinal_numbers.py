7d202a5a2dfc4382a2e3f534d3a13af5bb4eecef
fhjerten <f.hjerten@gmail.com>
2025-09-30 11:55:38 -0400

    Add files via upload

---
+++ assignments/05_if_statements/05_11_ordinal_numbers.py
@@ -0,0 +1,13 @@
+# I store the numbers 1 to 9 in a list
+
+numbers = list(range(1, 10))
+
+for number in numbers:
+    if number == 1:
+        print("1st")
+    elif number == 2:
+        print("2nd")
+    elif number == 3:
+        print("3rd")
+    else:
+        print(f"{number}th")
