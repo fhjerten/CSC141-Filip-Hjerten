4e93ffebf96fdfd9bfe7ad36822b24e566856702
fhjerten <f.hjerten@gmail.com>
2025-09-30 11:11:09 -0400

    Add files via upload

---
+++ assignments/05_if_statements/05_03_alien_colors_1.py
@@ -0,0 +1,12 @@
+
+# Version that passes the test
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")   # This will run
+
+# Version that fails the test
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")   # This will NOT run
