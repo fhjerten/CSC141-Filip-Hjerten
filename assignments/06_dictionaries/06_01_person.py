ac1f7a790eb398df55170a8f92bb2a71b788cb6c
fhjerten <f.hjerten@gmail.com>
2025-10-02 11:32:52 -0400

    Add files via upload

---
+++ assignments/06_dictionaries/06_01_person.py
@@ -0,0 +1,12 @@
+# Start of dictionary
+
+person = { 'first_name': "Filip",
+           'last_name': "Hjerten",
+           'age': 19,
+           'city': "Stockholm"
+           }
+# Print dictionary values
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
