a4fd455f77f994848db6253149935b2305a71de8
fhjerten <f.hjerten@gmail.com>
2025-10-02 11:47:42 -0400

    Add files via upload

---
+++ assignments/06_dictionaries/06_02_favorite_numbers.py
@@ -0,0 +1,5 @@
+favorite_numbers = {'Alice' : 2, 'Jerry' : 7, 'Bob' : 3, 'Eve' : 11, 'Charlie' : 5}
+
+for name, number in favorite_numbers.items():
+    print(f"{name}'s favorite number is {number}.")
+    
