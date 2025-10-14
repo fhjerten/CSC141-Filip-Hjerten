db62d1bffcf0e2e97610dc9f529e8731766cedc8
fhjerten <f.hjerten@gmail.com>
2025-10-14 14:37:48 -0400

    Add files via upload

---
+++ assignments/07_user_input_and_while_loops/07_04_pizza_toppings.py
@@ -0,0 +1,8 @@
+
+topping = ""
+
+while topping != "quit":
+    topping = input("Enter a pizza topping (or 'quit' to stop): ")
+    
+    if topping != "quit":
+        print(f"I'll add {topping} to your pizza!")
