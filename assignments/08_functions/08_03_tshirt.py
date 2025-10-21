4e42115e330a3211c5d3587ab803c6fd38c8cae7
fhjerten <f.hjerten@gmail.com>
2025-10-21 11:29:14 -0400

    Add files via upload

---
+++ assignments/08_functions/08_03_tshirt.py
@@ -0,0 +1,9 @@
+def make_shirt(size, message):
+
+    print(f"\nI'm going to make a {size}-sized shirt with the message '{message}' printed on it.")
+
+# I call the function with keyword arguments
+make_shirt('large', 'Keep Calm and Code Python')
+
+# I call the function with keyword arguments
+make_shirt(size='medium', message='Hello World')
