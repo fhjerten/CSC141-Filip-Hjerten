1544f172f2e62c5f655e48471ae1c6597ede8055
fhjerten <f.hjerten@gmail.com>
2025-10-21 11:45:18 -0400

    Add files via upload

---
+++ assignments/08_functions/08_09_messages.py
@@ -0,0 +1,14 @@
+def show_messages(messages):
+
+# I print each message from a list of messages
+    for message in messages:
+        print(message)
+
+text_messages = [
+    "Hey there!",
+    "How are you?",
+    "Let's play golf tomorrow!",
+    "Good luck on your test!"
+]
+
+show_messages(text_messages)
