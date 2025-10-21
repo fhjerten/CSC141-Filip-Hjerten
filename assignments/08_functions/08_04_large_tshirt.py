39a10e816596e1b018fee7bc92b7d7e34d3cf8cf
fhjerten <f.hjerten@gmail.com>
2025-10-21 11:31:12 -0400

    Add files via upload

---
+++ assignments/08_functions/08_04_large_tshirt.py
@@ -0,0 +1,12 @@
+def make_shirt(size='large', message='I love Python'):
+
+    print(f"\nI'm going to make a {size}-sized shirt with the message '{message}' printed on it.")
+
+# I make a large shirt with the message
+make_shirt()
+
+# Make a medium shirt with the message
+make_shirt(size='medium')
+
+# Make a shirt of any size with a different message
+make_shirt('small', 'Code. Sleep. Repeat.')
