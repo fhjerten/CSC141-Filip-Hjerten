be6ff4ff03d320e38ad60436531f90a7a099da3b
fhjerten <f.hjerten@gmail.com>
2025-10-23 12:18:39 -0400

    Add files via upload

---
+++ assignments/09_classes/09_01_restaurant.py
@@ -0,0 +1,23 @@
+# I start with defining the Restaurant class
+class Restaurant:
+    # I make the initializer method
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    # I make a method to describe the restaurant
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name} serves {self.cuisine_type} cuisine.")
+
+    # Method saying it is opened
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is now open for all Python customers!")
+
+# Type of restaurant class
+restaurant = Restaurant("Pasta Palace", "Italian")
+
+print(restaurant.restaurant_name)
+print(restaurant.cuisine_type)
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
