6ddc88305c06c73ca16a78fa02f20b6d7ddba3b8
fhjerten <f.hjerten@gmail.com>
2025-10-28 21:12:11 -0400

    Delete 09_10_imported_restaurant.py directory

--- 09_10_imported_restaurant.py/my_restaurant.py
+++
@@ -1,8 +0,0 @@
-# This file imports the Restaurant class from restaurant.py
-from restaurant import Restaurant
-
-# Make a Restaurant instance
-my_restaurant = Restaurant('Pasta Palace', 'Italian')
-
-# Call one of its methods to confirm the import works
-my_restaurant.describe_restaurant()
