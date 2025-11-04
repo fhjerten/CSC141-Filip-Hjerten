688d7617afec3f967faad6591091a5999740198f
Filip Hjerten <f.hjerten@gmail.com>
2025-11-04 08:15:25 -0500

    Add files via upload

---
+++ assignments/10_files_and_exceptions/10_05_guest_book.py
@@ -0,0 +1,12 @@
+print("Enter 'quit' to stop adding names.")
+
+# Keeps asking the user to add names until they type 'quit'Phi
+while True:
+    name = input("What is your name? ")
+
+    if name.lower() == 'quit':
+        break
+    else:
+        with open('guest_book.txt', 'a') as file_object:
+            file_object.write(name + "\n")
+        print(f"Welcome, {name}! Your name has been added to the guest book.")
