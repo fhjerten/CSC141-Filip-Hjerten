d95f4396b3ec0e6267a3cb8f39662a2a09b45056
Filip Hjerten <f.hjerten@gmail.com>
2025-11-04 08:48:04 -0500

    Add files via upload

---
+++ assignments/10_files_and_exceptions/10_12_favorite_number_remembered.py
@@ -0,0 +1,14 @@
+import json
+
+filename = 'favorite_number.json'
+
+try:
+    with open(filename) as f:
+        number = json.load(f)
+except FileNotFoundError:
+    number = input("What is your favorite number? ")
+    with open(filename, 'w') as f:
+        json.dump(number, f)
+    print("Your favorite number has been saved.")
+else:
+    print(f"I know your favorite number! It's {number}.")
