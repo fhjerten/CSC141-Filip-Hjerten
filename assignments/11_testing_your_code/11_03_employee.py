9a6f70fcb0e1994b796543e7bb3cd17cc95fe629
Filip Hjerten <f.hjerten@gmail.com>
2025-11-04 11:21:26 -0500

    Add files via upload

---
+++ assignments/11_testing_your_code/11_03_employee.py
@@ -0,0 +1,15 @@
+import pytest
+from employee import Employee
+
+@pytest.fixture
+def employee():
+# Creating employee test fixture
+    return Employee('Filip', 'Hjerten', 50000)
+
+def test_give_default_raise(employee):
+    employee.give_raise()
+    assert employee.annual_salary == 55000
+
+def test_give_custom_raise(employee):
+    employee.give_raise(10000)
+    assert employee.annual_salary == 60000
